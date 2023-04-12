0440784526c637c630c3fa59a109aca490c76b1e
Júlio de Oliviera <ramosju423@gmail.com>
2023-04-12 18:04:11 -0300

    Estilização do site concluída
    Foi feita toda a parte de estilização do site, o próxima problema a ser resolvido é a questão da responsividade, principalmente para celulares

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// PROBLEMA: Na hora de adicionar, os outros itens são desmarcados. SOLUÇÃO: Adicionar os itens que foram marcados em uma lista e assim, verificar, se foi marcado antes, recebe valor checked.
+
 var outter = document.querySelector("div#output")
 var userText = document.querySelector("input#user")
 var items = document.querySelector("input#check")
 
 function add() {
-   identification++
+
    outter.innerHTML += `<br><input type="checkbox" name="check" id="check"> ${userText.value} `
 
    userText.value = ""
